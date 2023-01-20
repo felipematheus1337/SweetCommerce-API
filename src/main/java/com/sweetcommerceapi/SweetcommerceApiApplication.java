9508d6b684ced2a4171c51c38fa913375c1396cb
felipematheus1337 <lipehbr@gmail.com>
2023-01-19 22:12:40 -0300

    Projeto criado

---
+++ src/main/java/com/sweetcommerceapi/SweetcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.sweetcommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetcommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetcommerceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sweetcommerceapi/SweetcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweetcommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
